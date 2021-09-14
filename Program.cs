df9db002035bcb8333e26a6cd0717f65d6d9490a
Lera <megalera1913@gmail.com>
2021-09-14 12:17:12 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_1_lera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
